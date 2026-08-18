1c1320b93a74743a64a28adb5e70bb19675811f4
TheDoctorTM <92531876+The-DoctorTM@users.noreply.github.com>
2026-08-18 16:59:35 +0800

    Bug Fixes

--- Assets/Scripts/Client/Base Utility Scripts/Util/Singleton/Singleton.cs
+++ Assets/Scripts/Client/Base Utility Scripts/Util/Singleton/Singleton.cs
@@ -106,6 +106,8 @@ namespace Utility
         private static T FindInstance()
         {
             instance = FindAnyObjectByType<T>();
+            DontDestroyOnLoad(instance);
+
             if (instance != null) return instance;
             if (!Application.isPlaying) return instance;
             if (instance == null) Debug.LogError($"Networked Singletons cannot be created at run time! ({typeof(T).Name})");
